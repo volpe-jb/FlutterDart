c8ca02a4faf9b4867e8aabf5b85d6d22a5d4ec5d
JB <volpe-jb@users.noreply.github.com>
2018-07-16 04:47:42 -0500

    Add files via upload

---
+++ ClickMe_Streams/click_me.dart
@@ -0,0 +1,17 @@
+// used with DartPad
+
+import 'dart:html';
+
+void main() {
+  final ButtonElement button = querySelector('button');
+  
+  button.onClick
+    .timeout(
+      new Duration(seconds: 1),
+      onTimeout: (sink) => sink.addError('You lost!!!')
+    )
+    .listen(
+      (event) {},
+      onError: (err) => print(err)
+    );
+}
---
+++ GuessWord_Streams/wordguess_streams.dart
@@ -0,0 +1,16 @@
+// used with DartPad
+
+import 'dart:html';
+
+void main() {
+  final ButtonElement button = querySelector('button');
+  final InputElement input = querySelector('input');
+  
+  button.onClick
+    .take(4)
+    .where((event) => input.value == 'banana')
+    .listen(
+      (event) => print('You got it!'),
+      onDone: () => print('Nope. Bad guesses.')
+    );
+}
